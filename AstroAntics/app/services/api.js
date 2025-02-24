e55c0db855a9e96432b8bd2b2960b7d832a5f61c
sidd84755 <sidd84755@gmail.com>
2025-02-24 20:31:45 +0530

    Worked on api endpoint

--- AstroAntics/app/services/api.js
+++ AstroAntics/app/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://astroantics.onrender.com/api',
 });
 
 api.interceptors.request.use(async (config) => {
